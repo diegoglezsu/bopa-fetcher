a11771c2ba0070c510937b0be0d8bce1627cf684
Diego González Suárez <gonzalezsdiego@uniovi.es>
2026-07-01 15:38:32 +0200

    Update authors reference

--- src/bopa/__init__.py
+++ src/bopa/__init__.py
@@ -5,8 +5,8 @@ of the region of Asturias, Spain. This library provides programmatic access
 to its bulletins and articles via web scraping.
 """
 
-__author__ = ["Diego González Suárez", "Noelia Rico"]
-__email__ = ["gonzalezsdiego@uniovi.es", "noeliarico@uniovi.es"]
+__author__ = ["Diego González Suárez", "Noelia Rico", "Irene Díaz"]
+__email__ = ["gonzalezsdiego@uniovi.es", "noeliarico@uniovi.es", "sirene@uniovi.es"]
 __version__ = "0.1.6"
 
 from . import service
