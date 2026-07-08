c9dc82321dea5a977f69f8a53700ea0ca41dcb20
Diego González Suárez <gonzalezsdiego@uniovi.es>
2026-07-08 09:28:31 +0200

    chore: bump to version 0.1.8

--- src/bopa/__init__.py
+++ src/bopa/__init__.py
@@ -7,7 +7,7 @@ to its bulletins and articles via web scraping.
 
 __author__ = ["Diego González Suárez", "Noelia Rico", "Irene Díaz"]
 __email__ = ["gonzalezsdiego@uniovi.es", "noeliarico@uniovi.es", "sirene@uniovi.es"]
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 from . import service
 from . import api
